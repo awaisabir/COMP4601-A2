4038087a20c221cb2867231f870acd93083f8084
Pierre <pseguin2011@hotmail.com>
2018-03-24 18:22:43 -0400

    Added Comments and testing data

--- src/edu/carleton/comp4601/categories/Categorizer.java
+++ src/edu/carleton/comp4601/categories/Categorizer.java
@@ -2,11 +2,8 @@ package edu.carleton.comp4601.categories;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import weka.attributeSelection.Ranker;
 import weka.classifiers.trees.RandomForest;
 import weka.core.Attribute;
 import weka.core.DenseInstance;
@@ -15,15 +12,20 @@ import weka.core.Instances;
 
 
 public class Categorizer {
-	String[] movieGenre = {"Action","Comedy","Horror"};	
+	//array containing the genres of movies we are looking for. ADD HERE for more genres to determine
+	static String[] MOVIE_GENRE = {"Action","Comedy","Horror"};	
 	HashMap<String, Integer> wordCount;
 	int counter = 0;
 	RandomForest rfClassifier = null;
-	Ranker ranker = null;
-
-	Instances ins = null;
+	static Instances ins = null;
 	
+	/* 
+	 * Function:	Constructor
+	 * Purpose:		Finds all unique words (first 100 words) for all documents
+	 * 
+	 */
 	public Categorizer() {
+		//get all review content from database
 		String[] reviews = {"Here I Am!", "What are you doing", "Where do you go", "What did you think was going to happen", "Writing a review"};
 		wordCount = new HashMap<String, Integer>();
 		for(String review: reviews) {
@@ -37,10 +39,10 @@ public class Categorizer {
 		rfClassifier = new RandomForest();
 		train();
 	}
-	
+
 	public static void main(String[] args){
 		Categorizer c = new Categorizer();
-		HashMap<String,Integer> words = c.getPageWords("ABC");
+//		HashMap<String,Integer> words = c.getPageWords("ABC");
 //		String keyList = "", valList = "";
 //		for(String key: words.keySet()){
 //			keyList += key + "\t";
@@ -49,7 +51,16 @@ public class Categorizer {
 //		System.out.println(keyList);
 //		System.out.println(valList);
 	}
+	
+	
+	/*
+	 * Function: Train
+	 * Parameters: none
+	 * Purpose: takes the "Training" data, searches the database for the requested page
+	 *  		and trains the classifier with the preset result 
+	 */
 	public void train() {
+		//predetermined classification of movies made by Pierre Seguin **ADD MORE to improve accuracy**
 		HashMap<String,Integer> trainingData = new HashMap<String,Integer>();
 		trainingData.put("0784010331", 0);
 		trainingData.put("0792158288", 0);
@@ -63,24 +74,49 @@ public class Categorizer {
 		trainingData.put("B004RE29T0", 2);
 		trainingData.put("B0000DK4QJ", 0);
 		
-		HashMap<String, Integer> dataToTrain = getPageWords("a");
-		
-		weka.filters.supervised.attribute.AttributeSelection as = new  weka.filters.supervised.attribute.AttributeSelection();
-	    ranker = new Ranker();
-	    
+		HashMap<String, Integer> dataToTrain = new HashMap<String,Integer>();
+		dataToTrain.put("B004RE29T0", -1);
+	    dataToTrain.put("B00004RJ74", -1);
+	    dataToTrain.put("0792140923", -1);
 	    buildClassifier(trainingData, dataToTrain);
 
 	}
 	
+	/*
+	 * Function: 	getPageWords
+	 * Parameters: 	String pageID
+	 * Returns:		HashMap will ALL words and the count of their occurrences
+	 * 
+	 * Purpose:		Queries the database for the page content and stores all the counts of
+	 * 				word occurrences to the HashMap 
+	 */
 	public HashMap<String,Integer> getPageWords(String pageID) {
 		HashMap<String,Integer> pageCounts = new HashMap<String,Integer>(wordCount); 
-		String page = "I thought that the review of the following was great we are so great"; //get pageHTML
-		for(String key: pageCounts.keySet()){
-			pageCounts.put(key, page.split("\\s?"+key+"\\s").length - 1);
+		String page;
+		//Retrieves the pageID Content 
+		switch(pageID) {
+			case "B004RE29T0":
+				page = "I thought that the review of the following was great we are so great"; //get pageHTML
+				break;
+			default:
+				page = "What are you doing";
+				break;
 		}
+		
+		
+		//searches all words and counts the occurrences
+		for(String key: pageCounts.keySet())
+			pageCounts.put(key, page.split("\\s?"+key+"\\s").length - 1);
 		return pageCounts;
 	}
 	
+	/*
+	 * Function: 	buildClassifier
+	 * Parameters: 	trainingData - HashMap<PageID,PredictionValues>, unknownValues
+	 * 
+	 * Purpose:		searches all the pages to train, and 'teaches' the remaining pages based
+	 * 				on the training data
+	 */
 	public void buildClassifier(HashMap<String, Integer> trainingData, HashMap<String, Integer> unknownValues) {
 
 		try {
@@ -88,7 +124,7 @@ public class Categorizer {
 			rfClassifier.buildClassifier(training);
 			for(String key: unknownValues.keySet()){
 				unknownValues.put(key,(int)testData(getPageWords(key)));
-				System.out.println(unknownValues.get(key));
+				System.out.println(key + ": " + unknownValues.get(key));
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -97,60 +133,87 @@ public class Categorizer {
 		
 	}
 	
+	/*
+	 * Function:	buildTrainingData
+	 * Parameters:	trainingData - HashMap<PageID, PredictionValue>
+	 * Returns:		Instances object containing all the training data
+	 * 
+	 * Purpose:		trains the classifier based on the word counts in the page and the
+	 * 				preset results specified in train()
+	 */
 	Instances buildTrainingData(HashMap<String,Integer> trainingData){
+		//gets data structure
 		Instances newData = getInstancesOf(wordCount);
 		
 		int i = 0;
+		//goes through the pages
 		for(String key: trainingData.keySet()){
+			//retrieves the dataset with the word counts of the pages specified
 			HashMap<String,Integer> words = getPageWords(key);
+			//creates a new Instance to train with
 			newData.add(new DenseInstance(words.size()+1));
+			//sets all the count of values
 			for(String word: words.keySet())
 				newData.get(i).setValue(newData.attribute(word),words.get(word).intValue());
-			newData.get(i).setValue(ins.attribute("TheMovieGenre"),movieGenre[trainingData.get(key).intValue()]);
+			//sets the predetermined result
+			newData.get(i).setValue(ins.attribute("TheMovieGenre"), MOVIE_GENRE[trainingData.get(key).intValue()]);
 			i++;
 		}
 		return newData;
 	}
 
 	
-	
-	double testData(HashMap<String, Integer> unknownValues){
-//		ins.attribute(key)
-		
+	/*
+	 * Function:	testData
+	 * Parameters:	unknownValues - HashMap
+	 * Returns:		the index of the genre determined from the data
+	 * 
+	 * Purpose:		Determines the classification of a SINGLE unknown page
+	 * 				(HashMap contains the count of all the words in that page)
+	 */
+	int testData(HashMap<String, Integer> unknownValues){
+		//creates instance to classify
 		Instance newData = new DenseInstance(wordCount.size());
+		//specifies the structure
 		newData.setDataset(ins);
-		int j = 0;
-		for(String key: unknownValues.keySet()){
+		//sets all values
+		for(String key: unknownValues.keySet())
 			newData.setValue(ins.attribute(key),unknownValues.get(key).intValue());
-			j++;
-		}
+		//determines classification
 		double pred = 0;
 		try {
 			pred = rfClassifier.classifyInstance(newData);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return pred;
+		return (int)pred;
 	}
 	
 	
-	public Instances getInstancesOf(HashMap<String, Integer> wordCount){
+	/*
+	 * Function: 	getInstancesOf
+	 * Parameters: 	wordCount - HashMap 
+	 * 
+	 * Purpose: 	Creates an instance object designed to specify the structure 
+	 * 				of the classification data we use
+	 */
+	public static Instances getInstancesOf(HashMap<String, Integer> wordCount){
 		if (ins != null)
 			return ins;
-
+		
+		//adds the attributes associated to word counts
 		ArrayList<Attribute> attributes = new ArrayList<Attribute>();
 		for(String key: wordCount.keySet()) {
 			attributes.add(new Attribute(key));
 		}
 		
-		 // Declare a nominal attribute along with its values
-		 ArrayList<String> fvNominalVal = new ArrayList<String>(movieGenre.length);
-		 for(String genre: movieGenre)
+		 // Declares the possible results of the classification (Genre)
+		 ArrayList<String> fvNominalVal = new ArrayList<String>(Categorizer.MOVIE_GENRE.length);
+		 for(String genre: Categorizer.MOVIE_GENRE)
 			 fvNominalVal.add(genre);
 		 Attribute attribute3 = new Attribute("TheMovieGenre", fvNominalVal);
 		 attributes.add(attribute3);
-
+		 //specifies the result class is the "TheMovieGenre" attribute
 		 ins = new Instances("Rel",attributes, 10);
 		 ins.setClass(ins.attribute("TheMovieGenre"));
 		 return ins;
